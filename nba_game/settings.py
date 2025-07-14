ea8800694a9a9f1c9dab8e7d6447e6b9571e77fe
Abhlnaw <abhi@adom.inc?>
2025-07-14 06:33:36 -0500

    feat: added basic wheel functionality

--- nba_game/settings.py
+++ nba_game/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Static files directories for development
+STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
